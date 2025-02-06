c3671e6d87301dd7caa9e06b4a1057422537038c
Chaiyes <alessandro.chiaiese@libero.it>
2025-02-06 14:10:49 +0100

    Update country.py

--- dashboard/models/country.py
+++ dashboard/models/country.py
@@ -5,5 +5,5 @@ class Country(models.Model):
     name = models.CharField(max_length=256, default="Italia", verbose_name=_("Name"))
 
     class Meta:
-        verbose_name = "Country"
-        verbose_name_plural = "Countries" 
+        verbose_name = "Nation"
+        verbose_name_plural = "Nations" 
