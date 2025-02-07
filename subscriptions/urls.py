5181a50fe387ebfa45b68dc17def41e477412581
Chaiyes <alessandro.chiaiese@libero.it>
2025-02-07 17:26:39 +0100

    Updated

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # tests
     path('test/', views.test, name='subscriptions-test'),
-    path('plans/', views.plans, name='plans'),
+    # products
     path('products/purchased/', views.purchased_products, name='purchased_products'),
+    # subscriptions
+    path('plans/', views.plans, name='plans'),
     path('success/', views.success),
     path('cancel/', views.cancel),
 ]
