ca71e3df425e9fc5d5fff1ecd06aa018c4e83511
Chaiyes <alessandro.chiaiese@libero.it>
2025-02-11 21:47:49 +0100

    Updated

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # tests
     path('test/', views.test, name='subscriptions-test'),
     # products
+    path('products/', views.products, name='product_list'),
     path('products/purchased/', views.purchased_products, name='purchased_products'),
     # subscriptions
     path('plans/', views.plans, name='plans'),
