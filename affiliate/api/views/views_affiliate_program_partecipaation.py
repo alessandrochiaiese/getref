5b4a3961486758ada13933dd3fb1578b39d4e132
Chaiyes <alessandro.chiaiese@libero.it>
2025-03-02 14:11:41 +0100

    Update views_affiliate_program_partecipaation.py

--- affiliate/api/views/views_affiliate_program_partecipaation.py
+++ affiliate/api/views/views_affiliate_program_partecipaation.py
@@ -1,7 +1,7 @@
 from django.http import Http404
 from rest_framework.exceptions import NotFound
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework import status
