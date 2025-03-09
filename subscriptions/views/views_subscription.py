802e3ea3c0a007db02eeaef9abee0954dea84edf
Chaiyes <alessandro.chiaiese@libero.it>
2025-03-09 22:05:42 +0100

    Update views_subscription.py

--- subscriptions/views/views_subscription.py
+++ subscriptions/views/views_subscription.py
@@ -242,6 +242,7 @@ def create_checkout_session(request):
                 }
                 promotion = Promotion.objects.get(promotion_link=promotion_link)
                 selected_product = get_product_by_product_id(products, promotion.stripe_product_id)
+                price_id = selected_product.get('price_id')
             elif price_id:
                 selected_product = get_product_by_price_id(products, price_id)
             
